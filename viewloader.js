4a9a3295876fc1de8b625c8ecca9afe56bf150ba
Max Wheeler <max@icelab.com.au>
2014-09-10 11:23:59 +1000

    Add `includeScope` option
    Allows peeps to include the `$scope` element in the set of elements to execute view against (if it matches, obviously).

--- viewloader.js
+++ viewloader.js
@@ -5,11 +5,14 @@
   var dasherize = function(s) {
     return s.replace(/([a-z0-9])([A-Z])/g, "$1-$2").toLowerCase();
   };
-  viewloader.execute = function(views, $scope) {
+  viewloader.execute = function(views, $scope, includeScope) {
     for(var view in views) {
       var dashView = dasherize(view),
         selector = "[data-view-" + dashView + "]",
       $els = $scope ? $scope.find(selector) : $(selector);
+      if (includeScope === true && $scope) {
+        $els = $els.add($scope.filter(selector));
+      }
       $els.each(function(i, el) {
         var $el = $(el);
         views[view]($el, el, $el.data("view-" + dashView));
